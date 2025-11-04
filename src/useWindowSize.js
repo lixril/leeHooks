c6f44ed457bdeb7923b12297ff328b0ff2069eac
quddus-larik <quddus@proton.me>
2025-11-04 16:58:31 +0500

    update "export system"

--- src/useWindowSize.js
+++ src/useWindowSize.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 
-export default function useWindowSize() {
+export function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: 0,
     height: 0,
