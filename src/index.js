545a002b81f1659516290363faadba11423df47b
quddus-larik <quddus@proton.me>
2025-11-06 15:41:34 +0500

    renew for "useAsyncState"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // src/index.js
 
 export * from './useWindowSize';
-export * from './useActionState';
+export * from './useAsyncState';
 // Add an export line for every hook file you create
 // export * from './useNewHookName';
